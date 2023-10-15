4bb0958df8d8bd09a8a2bee69f94c1c432c3fa4d
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2023-10-15 09:02:01 +0530

    Update func_ptr_performance.cpp

--- func_ptr_performance.cpp
+++ func_ptr_performance.cpp
@@ -2,12 +2,26 @@
 #include<functional>
 #include <map>
 #include<iostream>
+#include <cstdlib> 
 
 #include <chrono>
 using namespace std::chrono;
  
 #define EXECUTIONS 50000000
-void conditionalAdd(long& i, long& j)
+void swseAdd(const int i, long& j)
+{
+    switch (i%2)
+    {
+        case 0:
+            j++;
+            break;
+        case 1:
+            j+=2;
+            break;
+    }
+}
+
+void conditionalAdd(const int i, long& j)
 {
     if(i%2 == 0)
     {
@@ -19,8 +33,8 @@ void conditionalAdd(long& i, long& j)
     }
 }
 
-void add_1_ptr(long& a) { a++; };
-void add_2_ptr(long& a) { a+=2; };
+inline void add_1_ptr(long& a) { a++; };
+inline void add_2_ptr(long& a) { a+=2; };
 
 int main(int argc, char * argv[])
 {  
@@ -42,17 +56,17 @@ int main(int argc, char * argv[])
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
-    std::cout << duration.count() / 1000000 << std::endl; // 0 seconds
+    std::cout << duration.count()  << std::endl; // 0 seconds
     std::cout << j << std::endl;
     
     j = 0;
     start = high_resolution_clock::now();
     for(long i = 0; i < EXECUTIONS; i++) {
-        map[i%2](j);
+        map[rand()%2](j);
     }
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
-    std::cout << duration.count() / 1000000 << std::endl; // 5 seconds
+    std::cout << duration.count()  << std::endl; // 5 seconds
     std::cout << j << std::endl;
 
     void (*func_1)(long&);
@@ -66,17 +80,27 @@ int main(int argc, char * argv[])
     j = 0;
     start = high_resolution_clock::now();
     for(long i = 0; i < EXECUTIONS; i++) {
-        func_ptr_map[i%2](j);
+        func_ptr_map[rand()%2](j);
     }
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
     std::cout << duration.count() / 1000000 << std::endl; // 4 seconds
     std::cout << j << std::endl;
     
+    // start = high_resolution_clock::now();
+    // for(long i = 0; i < EXECUTIONS; i++) {
+    //     swseAdd(i,j);
+    // }
+    // stop = high_resolution_clock::now();
+    // duration = duration_cast<microseconds>(stop - start);
+    // std::cout << duration.count()  << std::endl; // 0 seconds
+    // std::cout << j << std::endl;
+    
+    
     j = 0;
     start = high_resolution_clock::now();
     for(long i = 0; i < EXECUTIONS; i++) {
-        if(i%2 == 0)
+        if(rand()%2 == 0)
         {
             func_ptr_map[0](j);
         } 
@@ -93,7 +117,7 @@ int main(int argc, char * argv[])
     j = 0;
     start = high_resolution_clock::now();
     for(long i = 0; i < EXECUTIONS; i++) {
-        if(i%2 == 0)
+        if(rand()%2 == 0)
         {
             add_1_ptr(j);
         } 
